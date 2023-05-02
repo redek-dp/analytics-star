94455dd8fdf7a8a2cbfac6bdb224befd022909d3
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2023-05-02 13:51:48 -0300

    Update DateFilter.tsx

--- dashboard/components/DateFilter.tsx
+++ dashboard/components/DateFilter.tsx
@@ -10,24 +10,24 @@ import {
 import useDateFilter from '../lib/hooks/use-date-filter'
 
 const dateFilterOptions: DateRangePickerOption[] = [
-  { text: 'Today', value: DateFilterType.Today, startDate: new Date() },
+  { text: 'Hoje', value: DateFilterType.Today, startDate: new Date() },
   {
-    text: 'Yesterday',
+    text: 'Ontem',
     value: DateFilterType.Yesterday,
     startDate: moment().subtract(1, 'days').toDate(),
   },
   {
-    text: '7 days',
+    text: '7 dias',
     value: DateFilterType.Last7Days,
     startDate: moment().subtract(7, 'days').toDate(),
   },
   {
-    text: '30 days',
+    text: '30 dias',
     value: DateFilterType.Last30Days,
     startDate: moment().subtract(30, 'days').toDate(),
   },
   {
-    text: '12 months',
+    text: '12 meses',
     value: DateFilterType.Last12Months,
     startDate: moment().subtract(12, 'months').toDate(),
   },
@@ -41,7 +41,7 @@ export default function DateFilter() {
       <Popover className="relative h-4">
         <Popover.Button>
           <QuestionIcon className="text-secondaryLight" />
-          <div className="sr-only">What is the time zone used?</div>
+          <div className="sr-only">Qual é o fuso horário usado?</div>
         </Popover.Button>
 
         <Popover.Panel className="absolute bottom-6 -right-10 bg-secondary text-white text-xs font-light rounded py-1 px-2 z-[2] w-24">
