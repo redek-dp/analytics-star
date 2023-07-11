5599cc462511764c21c20a33135774f37d6e8f9d
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2023-07-11 12:05:13 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-##aaa
+# servdevanalytics
---
+++ v1/app.id.js
@@ -0,0 +1,2 @@
+var tokendd = 'p.eyJ1IjogImFjOGRiN2I1LWZiMmUtNDQ1NS05NjA5LTM0ZGI0YjU5YWFhNyIsICJpZCI6ICIzNjg0ZTg4NC1iYmQ1LTQxMGYtODQ4ZC03OWU4NmM4MGM2MTMifQ.Ff1U8Y37FkVyRNVruCtBt6SL_mBaCqFDwPOK1nPGOSs';
+document.write('<script defer src="https://unpkg.com/@tinybirdco/flock.js" data-host="https://api.us-east.tinybird.co" data-token="'+tokendd+'"></script>');
