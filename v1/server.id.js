23fd7bbef29c6a93dd9462dd4760ed3ea8cbce63
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2023-07-11 16:03:56 -0300

    1

--- v1/server.id.js
+++ v1/server.id.js
@@ -1,1 +1,2 @@
-document.write('<script async defer data-website-id="faac05aa-e4b4-4e04-a616-c6a953ff409b" src="https://devanalytics.vercel.app/script.js"></script>');
+var tokendd = 'p.eyJ1IjogImFjOGRiN2I1LWZiMmUtNDQ1NS05NjA5LTM0ZGI0YjU5YWFhNyIsICJpZCI6ICIzNjg0ZTg4NC1iYmQ1LTQxMGYtODQ4ZC03OWU4NmM4MGM2MTMifQ.Ff1U8Y37FkVyRNVruCtBt6SL_mBaCqFDwPOK1nPGOSs';
+document.write('<script defer src="https://unpkg.com/@tinybirdco/flock.js" data-host="https://api.us-east.tinybird.co" data-token="'+tokendd+'"></script>');
