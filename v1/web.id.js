3c189cb68550dd19873f2e594bd9fe5d33f900fb
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2023-07-11 16:14:12 -0300

    1

---
+++ v1/web.id.js
@@ -0,0 +1,5 @@
+/*
+devweb
+*/
+var tokendd = 'p.eyJ1IjogIjRkNTVkYWQ5LTUyZTctNDMzOC04ODA1LTJkM2QxYzcxODQxYSIsICJpZCI6ICIzOGM5Y2U5NS02MmZkLTRjZWYtOTUzMy0yZWVjYjJjMWZhZjcifQ.lmNSgiRAxpzwcbPoOwc5I7ql7WhmJl8xL5pdIkhq8Lw';
+document.write('<script defer src="https://unpkg.com/@tinybirdco/flock.js" data-host="https://api.us-east.tinybird.co" data-token="'+tokendd+'"></script>');
