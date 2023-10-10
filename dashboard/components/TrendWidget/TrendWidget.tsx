5704ac1058918f8467305b4545565c17c45d2aad
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2023-10-10 12:49:39 -0300

    2

--- dashboard/components/TrendWidget/TrendWidget.tsx
+++ dashboard/components/TrendWidget/TrendWidget.tsx
@@ -17,30 +17,7 @@ export default function TrendWidget() {
 
   return (
     <Widget>
-      <div className="flex items-center justify-between">
-        <Widget.Title>Users in last 30 minutes</Widget.Title>
-        <h3 className="text-neutral-64 font-normal text-xl">
-          {data?.totalVisits ?? 0}
-        </h3>
-      </div>
-      <Widget.Content
-        status={status}
-        loaderSize={40}
-        noData={!chartData?.length}
-        warning={warning?.message}
-      >
-        <BarChart
-          data={chartData}
-          index="Date"
-          categories={['Number of visits']}
-          colors={['blue']}
-          className="h-32"
-          showXAxis={false}
-          showYAxis={false}
-          showLegend={false}
-          showGridLines={false}
-        />
-      </Widget.Content>
+      
     </Widget>
   )
 }
